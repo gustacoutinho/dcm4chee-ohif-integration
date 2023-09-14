8099598d177f1d221e283fb169a95aff187dafd2
Gustavo <gustavocoutiinho@gmail.com>
2023-09-14 19:27:42 -0400

    removendo arquivos desnecessários

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Lê o conteúdo do arquivo default.js e define como uma variável de ambiente
-export APP_CONFIG=$(cat default.js)
-
-# Inicia o contêiner com o docker-compose
-docker-compose up -d
-# docker run -d -p 3000:80/tcp -e APP_CONFIG="$(cat default.js)" --name ohif-viewer-container ohif/app:v3.7.0-beta.62
